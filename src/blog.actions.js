aa67c9eb01994ab2aa9a38c8b101d1e71ac750c6
lockst3r <ushakovqq@gmail.com>
2021-04-02 04:17:00 +0300

    add

--- src/blog.actions.js
+++ src/blog.actions.js
@@ -1,8 +1,8 @@
 import * as postsGateway from './gateway';
 
-export const POSTS_LIST_RECIEVED: string = 'POSTS_LIST_RECIEVED';
+export const POSTS_LIST_RECIEVED = 'POSTS_LIST_RECIEVED';
 
-export const postsListRecived = (postsList: Array<string>) => {
+export const postsListRecived = postsList => {
   const action = {
     type: POSTS_LIST_RECIEVED,
     payload: {
@@ -13,19 +13,14 @@ export const postsListRecived = (postsList: Array<string>) => {
 };
 
 export const getPostsList = () => {
-  const thunkAction = function (dispatch: any) {
+  const thunkAction = function (dispatch) {
     postsGateway.fetchPostsList().then(postsList => dispatch(postsListRecived(postsList.data)));
   };
   return thunkAction;
 };
 
-interface IPostData {
-  title: string;
-  body: string;
-}
-
-export const createPost = (postData: IPostData) => {
-  const thunkAction = function (dispatch: any) {
+export const createPost = postData => {
+  const thunkAction = function (dispatch) {
     thunkAction.createPost(postData).then(() => dispatch(getPostsList()));
   };
   return thunkAction;
